aa36fe263c66db5fc7971aac9eabbcd0648ce80c
prodaptdemo <mahendran.m@prodapt.com>
2017-05-31 12:15:57 +0530

    update db string

--- appsettings.js
+++ appsettings.js
@@ -3,9 +3,9 @@ var appsettings = appsettings || {};
 (function (settings) {
     settings.Config = {
         DBConnection: {
-            host: 'process.env.MYSQL_HOST',
-            user: 'process.env.MYSQL_USER',
-            password: 'process.env.MYSQL_PASS',
+            host: "'"+process.env.MYSQL_HOST+"'",
+            user: "'"+process.env.MYSQL_USER+"'",
+            password: "'"+process.env.MYSQL_PASS+"'",
             database: 'microdb'
         }
     };
