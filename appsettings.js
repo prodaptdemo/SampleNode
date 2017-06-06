e59bc959bcd7056dad3bc90bd6b2a9bad1c057d0
prodaptdemo <mahendran.m@prodapt.com>
2017-06-06 18:06:07 +0530

    db-sqldev

--- appsettings.js
+++ appsettings.js
@@ -3,9 +3,9 @@ var appsettings = appsettings || {};
 (function (settings) {
     settings.Config = {
         DBConnection: {
-            host: process.env.DB6T_DOMAINNAME,
-            user: process.env.DB6T_DBUSERNAME,
-            password: process.env.DB6T_DBPASSWORD,
+            host: process.env.SQLDEV_DOMAINNAME,
+            user: process.env.SQLDEV_DBUSERNAME,
+            password: process.env.SQLDEV_DBPASSWORD,
             database: 'microdb'
         }
     };
